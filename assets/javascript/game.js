867a5dbed5babee8c5b352b122948ac115069c29
Cody Rush <codyrush@gmail.com>
2018-11-28 15:00:25 -0800

    included instructions, reformated script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,15 @@
 // set up the list of words
-    var wordList = ["vorlauf","sparge","grain","hops",
-                    "yeast","water","gravity","wort",
-                    "malt","stout","porter","pale ale","fermentation"];
+var wordList = ["vorlauf","sparge","grain","hops","yeast","water","gravity","wort","malt","stout",
+                "porter","pale ale","fermentation","ale","amber","bung","brewpub","lauter","pitch","mash tun"];
 
 // count how many words, console it
-    console.log("Words: " + wordList.length);
+console.log("Words: " + wordList.length);
 
 // select a random word from the wordList
-        var word = wordList[Math.floor(Math.random()*wordList.length)];
-        console.log("Word: " + word);
-        console.log("Word Length: " + word.length);
+var word = wordList[Math.floor(Math.random()*wordList.length)];
+
+// console the word
+console.log("Word: " + word);
+
+// console the character length of the word
+console.log("Word Length: " + word.length);
